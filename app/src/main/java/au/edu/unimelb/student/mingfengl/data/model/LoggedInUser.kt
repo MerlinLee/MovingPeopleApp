7d9a6b5edabaab3667acd06a1994ad6276d2d48f
Merlin Lee <mingfeng.lee@outlook.com>
2019-09-06 08:56:57 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/unimelb/student/mingfengl/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package au.edu.unimelb.student.mingfengl.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
---
+++ app/src/main/java/au/edu/unimelb/student/mingfengl/ui/login/UploadMediaActivity.kt
@@ -0,0 +1,13 @@
+package au.edu.unimelb.student.mingfengl.ui.login
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import au.edu.unimelb.student.mingfengl.R
+
+class UploadMediaActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_upload)
+    }
+}

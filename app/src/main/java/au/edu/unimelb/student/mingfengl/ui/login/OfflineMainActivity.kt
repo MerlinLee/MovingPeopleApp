bf15ba7aeca8df7bf1f2cd871f69ff537a63f79e
Merlin Lee <mingfeng.lee@outlook.com>
2019-09-09 12:20:32 +1000

    New Version Recording and Display on the VideoView

--- app/src/main/java/au/edu/unimelb/student/mingfengl/ui/login/OfflineMainActivity.kt
+++ app/src/main/java/au/edu/unimelb/student/mingfengl/ui/login/OfflineMainActivity.kt
@@ -3,8 +3,8 @@ package au.edu.unimelb.student.mingfengl.ui.login
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
@@ -18,20 +18,29 @@ class OfflineMainActivity  : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_offline_main)
-        video = findViewById<VideoView>(R.id.view_video)
-        btn_capture = findViewById<Button>(R.id.btn_capture)
+        video = findViewById(R.id.view_video)
+        btn_capture = findViewById(R.id.btn_capture)
         btn_capture.setOnClickListener{
             dispatchTakeVideoIntent()
         }
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
-            val videoUri: Uri? = intent.data
+            var videoUri = data?.data
+            if (videoUri != null) {
+                video.setVideoPath(videoUri.path)
+            }
             video.setVideoURI(videoUri)
+            if (video.visibility== View.GONE){
+                video.visibility=View.VISIBLE
+            }
+            video.start()
         }
     }
+
 
     private fun dispatchTakeVideoIntent() {
         Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
